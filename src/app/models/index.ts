809232965c92ac49f4af199f781d2f811deb1c3d
Edson Martins <edsonmartins3110@gmail.com>
2022-10-14 21:05:04 -0300

    feat: refatora tudo

--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -2,3 +2,5 @@ export * from './file.model';
 export * from './category.model';
 export * from './user.model';
 export * from './credential-user.model';
+export * from './simple-user.model';
+export * from './product.model';
---
+++ src/app/models/product.model.ts
@@ -0,0 +1,70 @@
+import { Category, CategoryDTO, FileDTO, SimpleUser, SimpleUserDTO, File } from '.';
+
+interface ProductDTO {
+  uid: string;
+  name: string;
+  description: string;
+  enable: boolean;
+  createdUser: SimpleUserDTO;
+  categories: CategoryDTO[];
+  images: FileDTO[];
+}
+
+export class Product {
+  #uid: string;
+  get uid(): string {
+    return this.#uid;
+  }
+
+  #name: string;
+  get name(): string {
+    return this.#name;
+  }
+
+  #description: string;
+  get description(): string {
+    return this.#description;
+  }
+
+  #enable: boolean;
+  get enable(): boolean {
+    return this.#enable;
+  }
+
+  #createdUser: SimpleUser;
+  get createdUser(): SimpleUser {
+    return this.#createdUser;
+  }
+
+  #categories: Category[];
+  get categories(): Category[] {
+    return this.#categories;
+  }
+
+  #images: File[];
+  get images(): File[] {
+    return this.#images;
+  }
+
+  constructor({ uid, name, description, enable, createdUser, categories, images }: ProductDTO) {
+    this.#uid = uid;
+    this.#name = name;
+    this.#description = description;
+    this.#enable = enable;
+    this.#createdUser = new SimpleUser(createdUser);
+    this.#categories = categories.map((c) => new Category(c));
+    this.#images = images.map((i) => new File(i));
+  }
+
+  toJson(): ProductDTO {
+    return {
+      uid: this.#uid,
+      name: this.#name,
+      description: this.#description,
+      enable: this.#enable,
+      createdUser: this.#createdUser.toJson(),
+      categories: this.#categories.map((c) => c.toJson()),
+      images: this.#images.map((i) => i.toJson()),
+    };
+  }
+}
