8fbd322900a3bba3c57a70f655efeaddb684a700
guilherme-growdev <guilherme.niches@growdev.com.br>
2023-03-06 15:10:17 -0300

    refactor: refatora rota de atualizar senha

--- src/app/features/account/controllers/update-password.controller.ts
+++ src/app/features/account/controllers/update-password.controller.ts
@@ -4,17 +4,23 @@ import { BcryptAdapter } from '@shared/adapters';
 import { appEnvironments } from '@envs/app.env';
 import { UpdatePassword } from '@account/usecases';
 import { AccountRepository } from '@account/repositories';
+import { LoadUserByLoginOrUidRepository } from '@shared/repositories';
 
 export class UpdatePasswordController {
   async updatePassword(request: Request, response: Response) {
     try {
-      const { password } = request.body;
+      const { newPassword, oldPassword } = request.body;
       const { userUid } = request.authUser;
       const bcrypt = new BcryptAdapter(appEnvironments.BCRYPT_SALT);
       const accountRepository = new AccountRepository();
-      const updatePassword = new UpdatePassword(bcrypt, accountRepository);
+      const loadUserByLoginOrUidRepository = new LoadUserByLoginOrUidRepository();
+      const updatePassword = new UpdatePassword(
+        bcrypt,
+        accountRepository,
+        loadUserByLoginOrUidRepository,
+      );
 
-      const result = await updatePassword.execute(password, userUid);
+      const result = await updatePassword.execute(newPassword, oldPassword, userUid);
 
       if (!result.success) return notOk(response, result);
 
