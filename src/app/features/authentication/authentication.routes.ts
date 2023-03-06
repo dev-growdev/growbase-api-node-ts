eba8773de98676d091c4e6089ebc38ef673b979e
guilherme-growdev <guilherme.niches@growdev.com.br>
2023-03-06 15:11:09 -0300

    feat: cria rota para manter usuario logado

--- src/app/features/authentication/authentication.routes.ts
+++ src/app/features/authentication/authentication.routes.ts
@@ -1,7 +1,11 @@
+import { AuthMiddleware } from '@shared/middlewares';
 import express from 'express';
-import { SignInController } from './controllers';
+import { AuthUserController, SignInController } from './controllers';
 import { SignInValidator } from './validators';
 
 export default (router: express.Router): void => {
+  const auth = new AuthMiddleware();
+
   router.post('/signin', new SignInValidator().handle, new SignInController().signIn);
+  router.get('/auth', auth.handle, new AuthUserController().authUser);
 };
